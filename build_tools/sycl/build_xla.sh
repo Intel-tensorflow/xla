472c932df874b2fe4e9bede30bfa55807578af07
Li, Guizi <guizi.li@intel.com>
2024-04-17 01:57:50 +0000

    update

---
+++ build_tools/sycl/build_xla.sh
@@ -0,0 +1,9 @@
+
+workspace=$1
+cd $workspace/xla 
+
+export SYCL_TOOLKIT_PATH=$workspace/oneapi/compiler/2024.1/
+export TF_L0_PATH=$workspace/level_zero/usr
+bazel_bin=$(ls $workspace/bazel/)
+./configure.py --backend=SYCL --host_compiler=GCC
+$workspace/bazel/$bazel_bin build --config=verbose_logs -s --verbose_failures --nocheck_visibility //xla/tools:run_hlo_module
