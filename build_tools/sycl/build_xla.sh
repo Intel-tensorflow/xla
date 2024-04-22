75cc9e27fc1184a90cddb53a5b48b4ecb70f445a
Li, Guizi <guizi.li@intel.com>
2024-04-22 06:11:14 +0000

    update

--- build_tools/sycl/build_xla.sh
+++ build_tools/sycl/build_xla.sh
@@ -2,7 +2,9 @@
 workspace=$1
 cd $workspace/xla 
 
-export SYCL_TOOLKIT_PATH=$workspace/oneapi/compiler/2024.1/
+if [ -z ${SYCL_TOOLKIT_PATH+x} ];
+  export SYCL_TOOLKIT_PATH=$workspace/oneapi/compiler/2024.1/
+fi
 bazel_bin=$(ls $workspace/bazel/)
 ./configure.py --backend=SYCL --host_compiler=GCC
 $workspace/bazel/$bazel_bin build --config=verbose_logs -s --verbose_failures --nocheck_visibility //xla/tools:run_hlo_module
--- build_tools/sycl/install_oneapi.sh
+++ build_tools/sycl/install_oneapi.sh
@@ -1,4 +1,5 @@
 
+if [ -z ${SYCL_TOOLKIT_PATH+x} ];
 workspace=$1
 action=$2
 echo "Install Intel OneAPI in $workspace/oneapi"
@@ -9,3 +10,6 @@ if ! [ -f $workspace/l_BaseKit_p_2024.1.0.596.sh ]; then
   wget https://registrationcenter-download.intel.com/akdlm/IRC_NAS/fdc7a2bc-b7a8-47eb-8876-de6201297144/l_BaseKit_p_2024.1.0.596.sh
 fi
 bash l_BaseKit_p_2024.1.0.596.sh -a -s --eula accept --action $action --install-dir $workspace/oneapi --log-dir $workspace/oneapi/log --download-cache $workspace/oneapi/cache --components=intel.oneapi.lin.dpcpp-cpp-compiler:intel.oneapi.lin.mkl.devel
+else
+  echo "SYCL_TOOLKIT_PATH set to $SYCL_TOOLKIT_PATH", skip install/remove oneAPI;
+fi
